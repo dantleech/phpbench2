d3343a78e285de8d0836aa4c1da07cdfb6c07a78
Daniel Leech <dan.t.leech@gmail.com>
2019-12-26 10:21:47 +0000

    Add iterations option to command

--- src/Extension/Sampler/Command/SampleCommand.php
+++ src/Extension/Sampler/Command/SampleCommand.php
@@ -4,10 +4,12 @@ namespace PhpBench\Extension\Sampler\Command;
 
 use DTL\Invoke\Invoke;
 use PhpBench\Bridge\Console\MethodToConsoleOptionsBroker;
+use PhpBench\Library\Sampler\Sampler;
 use PhpBench\Library\Sampler\SamplerLocator;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\StringInput;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -15,7 +17,7 @@ class SampleCommand extends Command
 {
     const ARG_SAMPLER = 'sampler';
     const ARG_PARAMETERS = 'parameters';
-
+    const OPT_SAMPLES = 'samples';
 
     /**
      * @var SamplerLocator
@@ -32,6 +34,7 @@ class SampleCommand extends Command
     {
         $this->addArgument(self::ARG_SAMPLER, InputArgument::REQUIRED, 'Sampler alias');
         $this->addArgument(self::ARG_PARAMETERS, InputArgument::IS_ARRAY, 'Sampler parameters');
+        $this->addOption(self::OPT_SAMPLES, 's', InputOption::VALUE_REQUIRED, 'Number of samples to take', 1);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
@@ -39,6 +42,18 @@ class SampleCommand extends Command
         $alias = (string)$input->getArgument(self::ARG_SAMPLER);
         $sampler = $this->locator->get($alias);
 
+        $options = $this->resolveSamplerOptions($input, $sampler);
+
+        for ($i = 0; $i < $input->getOption(self::OPT_SAMPLES); $i++) {
+            $results = Invoke::method($sampler, '__invoke', array_filter($options));
+            $output->write(json_encode($results->toArray()), true, OutputInterface::OUTPUT_RAW);
+        }
+
+        return 0;
+    }
+
+    private function resolveSamplerOptions(InputInterface $input, Sampler $sampler): array
+    {
         $input = new StringInput(implode(
             ' ',
             array_map(
@@ -50,11 +65,7 @@ class SampleCommand extends Command
         $converter = new MethodToConsoleOptionsBroker(get_class($sampler), '__invoke');
 
         $input->bind($converter->inputDefinition());
-
         $options = $converter->castOptions($input->getOptions());
-        $results = Invoke::method($sampler, '__invoke', array_filter($options));
-        $output->write(json_encode($results->toArray()), false, OutputInterface::OUTPUT_RAW);
-
-        return 0;
+        return $options;
     }
 }
